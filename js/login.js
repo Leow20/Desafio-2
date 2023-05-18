53761828b6df1efee3be971ab580ffda8ca1deab
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-05-18 12:03:40 -0300

    Página de Criação de conta

--- js/login.js
+++ js/login.js
@@ -1,50 +1,70 @@
 var arr = [];
-
-// function createCount(){
-//     localStorage.email = document.getElementById("email").value;
-//     localStorage.senha  = document.getElementById("senha").value;
-    
-//     const newCount = new Count( localStorage.email,  localStorage.senha);
-
-// }
+var emailCreated = false;
 
  function Count (email, senha) {
     event.preventDefault();
     this.email = email;
-    this.senha = senha;
-    
-//console.log(this)
-   
+    this.senha = senha;   
 }
 
  
 function createCount(){
-    event.preventDefault();
+   event.preventDefault();
    if (localStorage.meuArr){             
       arr = JSON.parse(localStorage.getItem('meuArr')); 
    }
+   console.log(arr.length)
+   let Email = document.getElementById("email").value;
+   let Senha = document.getElementById("senha").value;
+  
    
-   let novoItem = document.getElementById("email").value;
-   arr.push(novoItem);
-   document.getElementById("email").value = "";
-   localStorage.meuArr = JSON.stringify(arr);
-   console.log(arr)
+   if(Email === null || Email === ""){
+       return alert("Você Precisa Preencher o campo Email")
+    }
+
+    if(Senha === null || Senha === ""){
+        return alert("Você Precisa Preencher o campo PassWord")
+     }
+    
+    verifyCratedCount(Email, arr)
+   
+    if(!emailCreated){
+       const newCount = new Count( Email, Senha);
+       arr.push(newCount);
+       document.getElementById("email").value = "";
+       document.getElementById("senha").value = "";
+       localStorage.meuArr = JSON.stringify(arr);
+    }
+
+}
+    
+    function verifyCratedCount(Email, arr){
+    
+        console.log(Email)
+
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i].email !== Email){
+                console.log("Pode Criar")
+                console.log(arr[i].email)
+                emailCreated = false;
+            } else {
+                console.log("Pode nao")
+                console.log(arr[i].email)
+                emailCreated = true;
+            }
+        }
 }
 
-// function showItems(){
-//    let resultDIV = document.getElementById('d');
-//    resultDIV.innerHTML = "";
-//    if (localStorage.meuArr){             
-//       arr = JSON.parse(localStorage.getItem('meuArr')); 
-//    }
-   
-//    for(var i in arr){
-//       let p = document.createElement("p");
-//       p.innerHTML = arr[i];
-//       resultDIV.append(p);
-//    }
-// }
 
+function verifyLogin(Email, Senha, arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].email === Email && arr[i].senha === Senha){
+            alert("Logado")
+        } else {
+            console.log("Email ou senha invalidas")  
+        }
+    }
+}
 
 
 function clearItems(){
